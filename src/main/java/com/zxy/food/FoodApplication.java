36eabfc1c9de2b35ec074cdc9155b6038198d334
@z-xy8-88 <1821173814@qq.com>
2024-05-10 09:21:58 +0800

    zxy second commit

---
+++ src/main/java/com/zxy/food/FoodApplication.java
@@ -0,0 +1,15 @@
+package com.zxy.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 艾
+ * @Time 2024/5/9 4:56 PM
+ */
+@SpringBootApplication
+public class FoodApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoodApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zxy/food/ZxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxy.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
